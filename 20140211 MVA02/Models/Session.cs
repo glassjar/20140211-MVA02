43f830e5c5417740659715018f37763ea9253319
glassjar <aks2302s@gmail.com>
2014-02-11 12:14:15 +0000

    Files prior to delete

---
+++ 20140211 MVA02/Models/Session.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace _20140211_MVA02.Models
+{
+    public class Session
+    {
+        public Int32 SessionID { get; set; }
+
+        [Required]
+        public String Title { get; set; }
+
+        [Required]
+        [DataType(DataType.MultilineText)]
+        public String Abstract { get; set; }
+
+        public Int32 SpeakerID { get; set; }
+
+        public Speaker Speaker { get; set;}
+
+    }
+}
---
+++ 20140211 MVA02/Models/Speaker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+namespace _20140211_MVA02.Models
+{
+    class Speaker
+    {
+        public Int32 SpeakerID { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
+        [Display(Name = "Speaker")]
+        public String Name { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        public String EmailAddress { get; set; }
+
+        public virtual List<Session> Sessions { get; set; }
+    }
+}
